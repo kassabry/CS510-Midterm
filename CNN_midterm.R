c97d95b1c9ca12da226194982a51c52ed62c0402
Ryan Kassab <rkassab@chapman.edu>
2020-12-10 00:07:07 -0800

    Added installation of packages

--- CNN_midterm.R
+++ CNN_midterm.R
@@ -1,3 +1,11 @@
+install_keras(
+  method = c("auto", "virtualenv", "conda"),
+  conda = "auto",
+  version = "default",
+  tensorflow = "default",
+  extra_packages = c("tensorflow-hub")
+)
+
 library(tensorflow)
 library(keras)
 
